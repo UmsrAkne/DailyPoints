d94d25803527d5b5fb420ba3867783d7d87576af
UmsrAkne <sibata486@gmail.com>
2026-06-26 22:38:24 +0900

    init

---
+++ DailyPoints/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using DailyPoints.Views;
+using Prism.Ioc;
+
+namespace DailyPoints
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ DailyPoints/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using DailyPoints.Utils;
+using Prism.Mvvm;
+
+namespace DailyPoints.ViewModels;
+
+public class MainWindowViewModel : BindableBase
+{
+    #if DEBUG
+    // ReSharper disable once UnusedMember.Local
+    private readonly string testDirectoryPath =
+        Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            @"myFiles\tests\RiderProjects\DailyPoints");
+    #endif
+
+    private readonly AppVersionInfo appVersionInfo = new();
+
+    public MainWindowViewModel()
+    {
+        SetupDummyData();
+    }
+
+    public string Title => appVersionInfo.Title;
+
+    [Conditional("DEBUG")]
+    private void SetupDummyData()
+    {
+    }
+}
